9674b3574fd7615ee3e3a82ed608db9622cf1a2b
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2018-03-04 23:56:41 +0900

    gh-pages

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-8-puzzle/',
 
     /**
      * Source Maps
